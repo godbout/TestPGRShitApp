b8a82a57b05ebe4473c354f85bd313bab9142611
Guill Lo <guill.bout@gmail.com>
2021-12-21 19:18:18 +0800

    Initial Commit

---
+++ TestPGRShitApp/TestPGRShitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestPGRShitAppApp.swift
+//  TestPGRShitApp
+//
+//  Created by Guillaume Leclerc on 21/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct TestPGRShitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
